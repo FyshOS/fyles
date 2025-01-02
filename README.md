8bbcc94f27a76be6ac20ae3b8321ae5e846a3fb3
Andy Williams <andy@andy.xyz>
2025-01-02 20:05:52 +0000

    Support rendering multiple file panels
    Command line only for now

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 This repo contains the work-in-progress Fyne file browser.
 
 ![](img/screenshot.png)
+
+## Usage
+
+Open the Fyles app as a GUI or from the command line.
+You can pass command-line argument to specify which folder to open.
+
+## Multiple Panels
+
+By specifying more than one location the Fyles window will open
+with multiple panels displayed in a row.
+
+    fyles Code/myProject Code/myOldProject
--- main.go
+++ main.go
@@ -5,12 +5,14 @@ package main
 import (
 	"os"
 
+	"github.com/fyshos/fyles/pkg/fyles"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/storage"
-
-	"github.com/fyshos/fyles/pkg/fyles"
+	"fyne.io/fyne/v2/theme"
 )
 
 const winTitle = "Fyles"
@@ -21,23 +23,57 @@ func main() {
 	w := a.NewWindow(winTitle)
 	w.SetPadded(false)
 
-	path, _ := os.Getwd()
-	if len(os.Args) > 1 {
-		path = os.Args[1]
+	panels := []fyne.CanvasObject{}
+	addPanel := func(path string) {
+		current := storage.NewFileURI(path)
+		item := makePanel(current, w)
+		panels = append(panels,
+			container.NewStack(
+				canvas.NewRectangle(theme.BackgroundColor()),
+				item))
 	}
-	current := storage.NewFileURI(path)
+	path, _ := os.Getwd()
+	for i := 1; i < len(os.Args); i++ {
+		addPanel(os.Args[i])
+	}
+
+	if len(panels) == 0 {
+		addPanel(path)
+	}
+
+	bg := canvas.NewRectangle(theme.OverlayBackgroundColor())
+	w.SetContent(container.NewStack(bg,
+		container.NewGridWithColumns(len(panels), panels...)))
+	w.Resize(fyne.NewSize(float32(15+(540*len(panels))), 310))
+
+	changes := make(chan fyne.Settings)
+	go func() {
+		for range changes {
+			bg.FillColor = theme.OverlayBackgroundColor()
+			bg.Refresh()
+
+			panelColor := theme.BackgroundColor()
+			for _, p := range panels {
+				bg := p.(*fyne.Container).Objects[0]
+				bg.(*canvas.Rectangle).FillColor = panelColor
+				bg.Refresh()
+			}
+		}
+	}()
+	a.Settings().AddChangeListener(changes)
+	w.ShowAndRun()
+}
+
+func makePanel(dir fyne.URI, w fyne.Window) fyne.CanvasObject {
 	ui := &fylesUI{win: w, filter: filterHidden()}
 	tools := ui.makeToolbar()
 	ui.items = fyles.NewFylesPanel(ui.itemTapped, w)
 	ui.items.Filter = ui.filter
 	tapper := newDirTapPanel(ui)
 	ui.fileScroll = container.NewScroll(container.NewStack(tapper, ui.items))
-	ui.fileTree = ui.makeFilesPanel(current)
-	ui.setDirectory(current)
+	ui.fileTree = ui.makeFilesPanel(dir)
+	ui.setDirectory(dir)
 	mainSplit := container.NewHSplit(ui.fileTree, ui.fileScroll)
 	mainSplit.Offset = 0.3
-
-	w.SetContent(container.NewBorder(tools, nil, nil, nil, mainSplit))
-	w.Resize(fyne.NewSize(555, 310))
-	w.ShowAndRun()
+	return container.NewBorder(tools, nil, nil, nil, mainSplit)
 }
